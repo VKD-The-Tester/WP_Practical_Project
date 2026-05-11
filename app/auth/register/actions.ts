a754e753272318550661b788cd4e2dbfef1e9993
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-05-11 23:55:56 +0300

    The register is finalized.

--- app/auth/register/actions.ts
+++ app/auth/register/actions.ts
@@ -1,8 +1,12 @@
 "use server";
 
+import { hashPassword } from "@/app/utils/password";
+import { prisma } from "@/app/utils/prisma";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
 const registerSchema = z.object({
+  name: z.string().nonoptional({ error: "Name is required" }),
   email: z.email({ error: "Invalid email address" }).trim(),
   password: z
     .string()
@@ -11,4 +15,36 @@ const registerSchema = z.object({
     .trim(),
 });
 
-const register = async () => {};
+const register = async (prevState: any, formData: FormData) => {
+  const result = registerSchema.safeParse(Object.fromEntries(formData));
+
+  if (!result.success) {
+    return { errors: z.flattenError(result.error).fieldErrors };
+  }
+
+  const { name, email, password } = result.data;
+
+  const user = await prisma.user.findUnique({ where: { email: email } });
+
+  if (user) {
+    return {
+      errors: {
+        email: ["A user with this email already exists"],
+      },
+    };
+  }
+
+  const hashedPassword = await hashPassword(password);
+
+  await prisma.user.create({
+    data: {
+      name: name,
+      email: email,
+      password: hashedPassword,
+    },
+  });
+
+  return redirect("/auth/login");
+};
+
+export default register;
