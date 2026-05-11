20f9b897126d8956fbdbe185cef263592566a28d
Victor Dimitrov <victorkdimitrov@gmail.com>
2026-05-11 23:50:28 +0300

    The navigation bar component now includes conditional rendering.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,8 +1,13 @@
-"use client";
-
 import Link from "next/link";
+import { cookies } from "next/headers";
+import { decrypt } from "@/app/utils/session";
+import { logout } from "@/app/auth/login/actions";
 
-const NavBar = () => {
+const NavBar = async () => {
+  const cookieStore = await cookies();
+  const sessionCookie = cookieStore.get("session")?.value;
+  const session = await decrypt(sessionCookie);
+
   return (
     <nav className="bg-white shadow-md font-bold">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -15,30 +20,44 @@ const NavBar = () => {
             </Link>
           </div>
           <div className="flex items-center space-x-4">
-            <Link
-              href="/events"
-              className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Search
-            </Link>
-            <Link
-              href="/events/post"
-              className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Create
-            </Link>
-            <Link
-              href="/auth/login"
-              className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Login
-            </Link>
-            <Link
-              href="/auth/register"
-              className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
-            >
-              Register
-            </Link>
+            {session ? (
+              <>
+                <Link
+                  href="/events"
+                  className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Search Events
+                </Link>
+                <Link
+                  href="/events/create"
+                  className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Create Event{" "}
+                </Link>
+                <form action={logout}>
+                  <button
+                    type="submit"
+                    className="cursor-pointer text-red-600 hover:text-red-900 px-3 py-2 rounded-md text-sm font-medium"
+                  ></button>
+                </form>
+              </>
+            ) : (
+              <>
+                {" "}
+                <Link
+                  href="/auth/login"
+                  className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Login
+                </Link>
+                <Link
+                  href="/auth/register"
+                  className="text-gray-600 hover:text-gray-900 px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Register
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </div>
